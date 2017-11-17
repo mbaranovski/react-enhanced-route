052ed22d7f11c2090d0862f55c7ffad452c61c31
Michal Baranowski <michal@alefeducation.com>
2017-11-17 19:01:26 +0400

    Switch tests from describe -> it, to test(); Amends in wallabyjs config.

--- src/__tests__/EnhancedRoute.test.js
+++ src/__tests__/EnhancedRoute.test.js
@@ -1,66 +1,53 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
-import { EnhancedRoute as Route } from '../EnhancedRoute';
+import {EnhancedRoute as Route} from '../EnhancedRoute';
 
-describe('EnhancedRoute test', () => {
 
-  const SideBar = () => <div>Sidebar</div>;
-  const Header = () => <div>Header</div>;
-  const MainLayout = (props) => <div><Header/><SideBar/>{props.children}</div>;
+const SideBar = () => <div>Sidebar</div>;
+const Header = () => <div>Header</div>;
+const MainLayout = (props) => <div><Header/><SideBar/>{props.children}</div>;
 
-  const AdminPage = () => <div>Admin Page</div>;
-  const PublicPage = () => <div>Public Page</div>;
+const AdminPage = () => <div>Admin Page</div>;
+const PublicPage = () => <div>Public Page</div>;
 
-  let isAuthorized;
 
-  beforeEach(() => {
-    isAuthorized = true;
-  });
+test('render default route with provided component', () => {
+  const element = render(<Router><Route component={PublicPage}/></Router>);
 
-  it('should render default route with provided component', () => {
+  expect(element).toMatchSnapshot();
+});
 
-    const element = render(<Router><Route component={PublicPage}/></Router>);
+test('render route with provided layout and child component', () => {
 
-    expect(element).toMatchSnapshot();
-  });
+  const element = render(<Router><Route layout={MainLayout} component={PublicPage}/></Router>);
 
-  it('should render route with provided layout and child component', () => {
+  expect(element).toMatchSnapshot();
+});
 
-    const element = render(<Router><Route layout={MainLayout} component={PublicPage}/></Router>);
+test('render and allow access to protected route with provided component', () => {
+  const isAuthorized = true;
+  const element = render(<Router><Route auth={isAuthorized} component={AdminPage}/></Router>);
 
-    expect(element).toMatchSnapshot();
-  });
+  expect(element).toMatchSnapshot();
+});
 
-  it('should render and allow access to protected route with provided component', () => {
+test('render and allow access to protected route with provided component and layout', () => {
+  const isAuthorized = true;
+  const element = render(<Router><Route auth={isAuthorized} layout={MainLayout} component={AdminPage}/></Router>);
 
-    const element = render(<Router><Route auth={isAuthorized} component={AdminPage}/></Router>);
+  expect(element).toMatchSnapshot();
+});
 
-    expect(element).toMatchSnapshot();
-  });
+test('redirect to provided page when accessing protected route', () => {
+  const isAuthorized = false;
+  const element = render(<Router><Route auth={isAuthorized} redirect={'/login'} component={AdminPage}/></Router>);
 
-  it('should render and allow access to protected route with provided component and layout', () => {
+  expect(element).toMatchSnapshot();
+});
 
-    const element = render(<Router><Route auth={isAuthorized} layout={MainLayout} component={AdminPage}/></Router>);
+test('render default unauthorized page when accessing protected route', () => {
+  const isAuthorized = false;
+  const element = render(<Router><Route auth={isAuthorized} component={AdminPage}/></Router>);
 
-    expect(element).toMatchSnapshot();
-  });
-
-  it('should redirect to provided page when accessing protected route', () => {
-
-    isAuthorized = false;
-
-    const element = render(<Router><Route auth={isAuthorized} redirect={'/login'} component={AdminPage}/></Router>);
-
-    expect(element).toMatchSnapshot();
-  });
-
-  it('should render default unauthorized page when accessing protected route', () => {
-
-    isAuthorized = false;
-
-    const element = render(<Router><Route auth={isAuthorized} component={AdminPage}/></Router>);
-
-    expect(element).toMatchSnapshot();
-  });
-
+   expect(element).toMatchSnapshot();
 });
